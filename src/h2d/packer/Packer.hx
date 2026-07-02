1f79102c7e02e94a53ae7b08ca955562fb5fff31
realvirtu <103977942+realvirtu@users.noreply.github.com>
2026-07-01 20:30:16 -0400

    Initial commit

---
+++ src/h2d/packer/Packer.hx
@@ -0,0 +1,19 @@
+package h2d.packer;
+
+class Packer extends Drawable
+{
+    public function new(?parent:Object)
+    {
+        super(parent);
+    }
+
+    public function update(dt:Float)
+    {
+        
+    }
+
+    override function draw(ctx:RenderContext)
+    {
+        // Draw logic
+    }
+}
